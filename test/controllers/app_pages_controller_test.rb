384ec8310db93e8c4ff2efdf8d338bc4ce273bcb
Abdul12majid <yisaabdulmajid@gmail.com>
2024-12-26 14:37:18 +0100

    created app_apges/index file

---
+++ test/controllers/app_pages_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class AppPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get app_pages_index_url
+    assert_response :success
+  end
+end
